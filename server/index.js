8a982f8341000ec794b1821d52239b30fcbace32
thibault <thibault.malespine@orange.fr>
2024-10-16 15:22:33 +0200

    _

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use('/static',express.static(path.join(__dirname, '../public/static/')));
 
 app.get('/', (req, res) => {
-    res.sendFile('/home/tmalespi/projet-TechnoWeb-Immo/public/index.html');
+    res.sendFile(path.join(__dirname,'../public/index.html'));
 });
 
 app.listen(3000, () => {
