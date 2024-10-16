8b2b29ad8a2753d600a3c9bba2e1f82f096c9966
thibault <thibault.malespine@orange.fr>
2024-10-16 15:11:16 +0200

    initialisation server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+ // middleware pour servir les fichiers static
+app.use('/static',express.static(path.join(__dirname, '../public/static/')));
+
+app.get('/', (req, res) => {
+    res.sendFile('/home/tmalespi/projet-TechnoWeb-Immo/public/index.html');
+});
+
+app.listen(3000, () => {
+    console.log(`Server is running at http://localhost:3000`);
+});
