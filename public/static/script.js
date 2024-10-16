9ecca841113bd3866af25e20862175128fa582c3
AziGod <armandlecourt2003@gmail.com>
2024-10-16 15:41:12 +0200

    5254525

---
+++ public/static/script.js
@@ -0,0 +1,17 @@
+// Fonction pour afficher/masquer le formulaire
+
+function toggleForm() {
+    var form = document.getElementById("annonceForm");
+    if (form.style.display === "block") {
+        form.style.display = "none";
+    } else {
+        form.style.display = "block";
+    }
+}
+
+// Fonction pour masquer le formulaire après soumission
+function closeForm() {
+    var form = document.getElementById("annonceForm");
+    form.style.display = "none";
+    alert("Annonce soumise avec succès !");
+}
