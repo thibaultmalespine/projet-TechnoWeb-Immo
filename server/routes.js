35576735e618ae267e289a321b0f6f98a1f9c475
thibault malespine <thibault.malespine@orange.fr>
2024-11-05 15:57:46 +0100

    enregistrement dans la bdd depuis le formulaire

--- server/routes.js
+++ server/routes.js
@@ -8,11 +8,12 @@ router.post('/submit-annonce', async (req, res) => {
   const { titre, url_annonce, description, type, ville, prix, m2_habitable, m2_terrain, meuble, particulier_pro, garage, piscine } = req.body;
 
   const sql = `INSERT INTO Annonce (NomAnnonce, URLOriginale, Description, TypeDeBien, laVille, Prix, M2Habitable, M2Terrains, Meuble, ParticulierPro, Garage, Piscine) 
-              VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`;
-  const values = [titre, url_annonce, description, type, ville, prix, m2_habitable, m2_terrain, meuble, particulier_pro, garage, piscine];
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12)`;
 
+  const values = [titre, url_annonce, description, type, ville, prix, m2_habitable, m2_terrain, meuble, particulier_pro, garage, piscine];
+    console.log(values);
   try {
-    const [result] = await client.query(sql, values);
+    const result = await client.query(sql, values);
     console.log('Annonce insérée', result);
     res.send('Annonce ajoutée avec succès');
   } catch (err) {
