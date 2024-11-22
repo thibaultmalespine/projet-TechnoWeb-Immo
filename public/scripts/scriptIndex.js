19f444e0a287f9f9c6a246b47a359bf8348fadeb
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2024-11-22 13:22:39 +0100

    _

--- public/scripts/scriptIndex.js
+++ public/scripts/scriptIndex.js
@@ -1,3 +1,4 @@
+// Gère l'envoi du formulaire de connexion
 const loginForm = document.querySelector("#login-form");
 
 loginForm.addEventListener('submit',async (event) => {
@@ -8,10 +9,33 @@ loginForm.addEventListener('submit',async (event) => {
     const data = Object.fromEntries(formData.entries());
   
         const response = await fetch(`/compte/login?email=${data.email}&motDePasse=${data.motDePasse}`);
-        if (response.redirected) {
-            document.location.href = response.url
+        if (response.ok) {
+            document.location.href = '/pages/mesAnnonces.html'
         } else if(response.status === 404) {
             alert("compte non trouvé")
         }
+})
 
+// Gère l'envoi du formulaire de création de compte
+const signupForm = document.querySelector("#signup-form");
+
+signupForm.addEventListener('submit',async (event) => {
+    event.preventDefault()
+    
+    // Récupération des données du formulaire
+    const formData = new FormData(signupForm);
+    const data = Object.fromEntries(formData.entries());
+  
+        const response = await fetch(`/compte/submit`, {
+            method : 'POST',
+            headers : {
+                'content-type' : 'application/json'
+            },
+            body : JSON.stringify(data)
+        });
+        if (response.ok) {
+            document.location.href = '/pages/mesAnnonces.html'
+        } else if(response.status === 500) {
+            alert("erreur du serveur, veuillez réessayer plus tard")
+        }
 })
