b2837e645eba7a28aabaf5faffa405b355b77023
fromScratch-ToDev <145133454+fromScratch-ToDev@users.noreply.github.com>
2024-11-27 17:47:44 +0100

    ajout test de la route put (compte/update) de la table compte + renommage dans le fichier index.html de motDePasse en motdepasse (postgresql formate tout en minuscule)

--- server/modeles/modeleCompteClient.js
+++ server/modeles/modeleCompteClient.js
@@ -2,19 +2,19 @@ import { client } from '../bdd.js';
 
 const Compte = {
   get: async (data) => { 
-    const { email, motDePasse } = data;
-    const result = await client.query('SELECT * FROM Compte WHERE email = $1 and motdepasse = $2', [email, motDePasse]);
+    const { email, motdepasse } = data;
+    const result = await client.query('SELECT * FROM Compte WHERE email = $1 and motdepasse = $2', [email, motdepasse]);
     return result.rows[0]; // Retourne le premier résultat ou undefined si non trouvé
   },
   create: async (data) => {
-    const {email, motDePasse} = data;
-    const result = await client.query('INSERT INTO Compte (Email, MotDePasse) VALUES ($1, $2) RETURNING *', [email, motDePasse]);
+    const {email, motdepasse} = data;
+    const result = await client.query('INSERT INTO Compte (Email, motdepasse) VALUES ($1, $2) RETURNING *', [email, motdepasse]);
     return result.rows[0];
   },
   update: async (data) => {
-    const {idCompte, nom, prenom, email, motDePasse} = data;
-    const result = await client.query('UPDATE Compte SET Nom = $1, Prenom = $2, Email = $3, MotDePasse = $4 WHERE idCompte = $5 RETURNING *', 
-                                      [nom, prenom, email, motDePasse, idCompte]);
+    const {idCompte, nom, prenom, email, motdepasse} = data;
+    const result = await client.query('UPDATE Compte SET Nom = $1, Prenom = $2, Email = $3, motdepasse = $4 WHERE idCompte = $5 RETURNING *', 
+                                      [nom, prenom, email, motdepasse, idCompte]);
     return result.rows[0];
   },
   delete: async (idCompte) => {
