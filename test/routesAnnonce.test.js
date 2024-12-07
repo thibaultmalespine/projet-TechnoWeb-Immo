89cbd092e2043d7e805feab0634bb4faae8933e9
Your Name <you@example.com>
2024-12-07 23:51:46 +0100

    test

--- test/routesAnnonce.test.js
+++ test/routesAnnonce.test.js
@@ -3,14 +3,9 @@ import { client } from '../server/bdd';
 import Annonce from '../server/modeles/modeleAnnonce.js';
     
 
-describe("Tests des fonctions de la BDD", () => {
+describe("Tests des routes d'Annonce", () => {
 
-  // Annule la transaction après chaque test pour isoler les données
-  afterEach(async () => {
-    await client.query('ROLLBACK');
-  });
-
-  it("create devrait insérer une nouvelle annonce", async () => {
+  it("Test de la route submit pour insérer une annonce", async () => {
     // Préparation des données de test
     const body = {
       titre: 'Test Annonce',
@@ -29,65 +24,36 @@ describe("Tests des fonctions de la BDD", () => {
       lecompte: "armandlecourt2003@gmail.com",
     };
 
-    // Appelle la méthode create pour insérer les données
-    const result = await Annonce.create(body);
 
-    // Vérifie que l'annonce a bien été créée avec les bonnes données
-    expect(result).toBeDefined();
-    expect(result.NomAnnonce).toBe(body.titre);
-    expect(result.URLOriginale).toBe(body.url_annonce);
-    expect(result.Description).toBe(body.description);
-    expect(result.TypeDeBien).toBe(body.type);
-    expect(result.CodePostal).toBe(body.codep);
-    expect(result.NomVille).toBe(body.ville);
-    expect(result.Prix).toBe(body.prix);
-    expect(result.M2Habitable).toBe(body.m2_habitable);
-    expect(result.M2Terrains).toBe(body.m2_terrain);
-    expect(result.Meuble).toBe(body.meuble);
-    expect(result.ParticulierPro).toBe(body.particulier_pro);
-    expect(result.Garage).toBe(body.garage);
-    expect(result.Piscine).toBe(body.piscine);
-    expect(result.LeCompte).toBe(body.lecompte);
-  });
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  it("getAllAnnonces devrait retourner une liste non vide", async () => {
-    console.log("Debut du test");
-  
-    // Simuler une requête GET
-    const response = await fetch("http://localhost:3000/annonce/getAll", {
-      method: "GET",
+    // Simuler une requête POST
+    const response = await fetch("http://localhost:3000/annonce/submit", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+      body: JSON.stringify(body),
     });
-  
-    // Vérifier si la réponse est réussie (status 200)
-    expect(response.ok).toBe(true);
+    const data = await response.text();
+    //console.log(data);
 
-  
-    // Convertir la réponse en JSON
-    const data = await response.json();
-  
-    // Vérifier que la liste d'annonces n'est pas vide
-    expect(data.length).toBeGreaterThan(0);
+
+    //const submitData = await response.json();
+    //const annonceId = submitData.id; // Récupère l'ID de l'annonce créée pour le test update et delete
+
+    //console.log(annonceId)
     
-    await client.connect();
-    await client.query("DELETE FROM Annonce WHERE NomAnnonce = 'Test Annonce'");
-    await client.end(); 
+    expect(response.ok).toBe(true);
   }, 10000); // Augmentez le timeout à 10 secondes si nécessaire
-});
+
+
+
+
+
+
+
+
+
+
+
+
+  });
